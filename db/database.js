84973bcde169f8b2f9b859bb312a102393c9ef96
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-22 22:16:12 -0300

    feat(remove): remove all files nextjs

--- db/database.js
+++ db/database.js
@@ -1,14 +1,12 @@
 import mysql from "mysql2"
 
 export default function createConnection(){
-    const connection = mysql.createPool({
+    const connection = mysql.createConnection({
         host: "monorail.proxy.rlwy.net",
         port: 10733,
         user: "root",
         password: "Fbf1Cg3D3bB5BCF5gebGAF2d46-3D3-D",
         database: "railway"
     })
-
-    const pool = connection.promise()
-    return pool
+    return connection
 }
